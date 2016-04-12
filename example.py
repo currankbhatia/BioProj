777030c6fd799e453aec673c74a53ca7c913c228
curranb <currankbhatia@gmail.com>
2016-04-12 17:19:23 -0500

    adding example from biopython tutorial

---
+++ example.py
@@ -0,0 +1,12 @@
+#Found this example from http://biopython.org/wiki/Getting_Started
+from Bio.Seq import Seq
+
+#create a sequence object
+my_seq = Seq('CATGTAGACTAG')
+
+#print out some details about it
+print 'seq %s is %i bases long' % (my_seq, len(my_seq))
+print 'reverse complement is %s' % my_seq.reverse_complement()
+print 'protein translation is %s' % my_seq.translate()
+
+print 'find GTA %i' % my_seq.find("GTA")
